af7fa0735176a22a732ed6feb913b79fd88312cf
IsakBH <isak@brunhenriksen.net>
2025-08-30 15:38:19 +0200

    Lagt til forklarende kommentarer på hva koden gjør

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use simple_stopwatch::Stopwatch;
 use std::io;
 use std::time::Duration;
-use std::fmt::Display;
 
 fn main() {
-    println!("Er du klar for å skrive?");
+    // spør om brukeren er klar for å skrive
+    println!("Er du klar for å skrive? (Trykk enter)");
     let mut ready = String::new();
     io::stdin()
         .read_line(&mut ready)
         .expect("Failed to read line");
     println!("Brukeren er klar.");
 
+    // teller ned fra 5 slik at brukeren kan gjøre seg klar
     println!("5..");
     std::thread::sleep(Duration::from_millis(1000));
     println!("4..");
@@ -26,16 +27,22 @@ fn main() {
     let amount_of_words: Vec<_> = text.split(" ").collect();
     println!("Yo, du skal skrive dette:\n{text}");
     let stopwatch = Stopwatch::start_new();
+
+    // user input for den faktiske skrive delen
     let mut user_text = String::new();
     io::stdin()
         .read_line(&mut user_text)
         .expect("Failed to read line");
+
+    // henter hvor mye tid brukeren brukte på å skrive
     let user_time = stopwatch.s();
 
+    // hvis burkeren skrev riktig :)
     if user_text.trim_end() == text {
         println!("Du brukte {user_time} sekunder på å skrive det.");
         println!("Du skrev {:?} ord.", amount_of_words.len());
     }
+    // hvis brukeren skrev feil :(
     else {
         println!("Det du skrev matchet ikke det du skulle skrive. Bad boy.");
     }
